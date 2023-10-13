8de180c17f74f74834c09c8d41e49c1d3388365b
Fred78290 <frederic.boltz@gmail.com>
2023-10-13 15:34:47 +0200

    Change bundle method

--- index.js
+++
@@ -1,3 +0,0 @@
-export { default } from './dist/vue3-content-placeholders.common'
-export * from './dist/vue3-content-placeholders.common'
-import './dist/vue3-content-placeholders.css'
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,64 @@
-import { mergeConfig } from 'vitest/config'
-import viteConfig from './config/vite.config.base'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { terser } from "rollup-plugin-terser";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+const production = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
-export default mergeConfig(viteConfig)
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  build: {
+    cssMinify: false,
+    minify: production,
+    lib: {
+      // src/indext.ts is where we have exported the component(s)
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "Vue3ContentPlaceHolder",
+      // the name of the output files when the build is run
+      fileName: "vue3-content-placeholders",
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "./src/styles.scss";
+          `,
+        },
+      },
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      plugins: [
+        terser({
+          compress: {
+            defaults: false,
+            drop_console: true,
+          },
+          mangle: {
+            eval: true,
+            module: true,
+            toplevel: true,
+            safari10: true,
+            properties: false,
+          },
+          output: {
+            comments: false,
+            ecma: "2020",
+          },
+        }),
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
